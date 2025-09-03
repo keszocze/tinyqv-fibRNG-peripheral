d5ead14ed7a9ac16dec9d292a2a268e5652c09bd
Oliver Keszöcze <info@oliver.keszoecze.de>
2025-09-03 08:51:05 +0000

    Rename design

--- src/fibRNG.v
+++ src/fibRNG.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Your Name
+ * Copyright (c) 2025 Oliver Keszocze
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -8,11 +8,11 @@
 // Change the name of this module to something that reflects its functionality and includes your name for uniqueness
 // For example tqvp_yourname_spi for an SPI peripheral.
 // Then edit tt_wrapper.v line 38 and change tqvp_example to your chosen module name.
-module tqvp_example (
+module tqvp_fibRNG (
     input         clk,          // Clock - the TinyQV project clock is normally set to 64MHz.
     input         rst_n,        // Reset_n - low to reset.
 
-    input  [7:0]  ui_in,        // The input PMOD, always available.  Note that ui_in[7] is normally used for UART RX.
+    input  [7:0]  ui_in,         // The input PMOD, always available.  Note that ui_in[7] is normally used for UART RX.
                                 // The inputs are synchronized to the clock, note this will introduce 2 cycles of delay on the inputs.
 
     output [7:0]  uo_out,       // The output PMOD.  Each wire is only connected if this peripheral is selected.
